4c891e046f17dc9127b2c01e89876905e0d9b2fc
Ivan Volik <i.a.volik@gmail.com>
2020-12-08 20:05:38 +0300

    generate components & payment model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
